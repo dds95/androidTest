bd1da4695fd63ad09dbf0bc6979daac9d7ca991e
dds95 <513876378@qq.com>
2016-01-08 09:12:58 +0800

    增加集成类

---
+++ src/com/example/androidtest/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.androidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
